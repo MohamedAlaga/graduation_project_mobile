1b1fc238fd7fdf57a20f368d33c5b684c3156094
MohamedAlaga <malaga.142014@gmail.com>
2024-05-30 18:17:56 +0300

    remove getignore

---
+++ build/rive_common/intermediates/cxx/Debug/186u5d11/logs/x86_64/configure_command.bat
@@ -0,0 +1,18 @@
+@echo off
+"C:\\Users\\MohamedAlaga\\AppData\\Local\\Android\\sdk\\cmake\\3.18.1\\bin\\cmake.exe" ^
+  "-HC:\\Users\\MohamedAlaga\\AppData\\Local\\Pub\\Cache\\hosted\\pub.dev\\rive_common-0.2.8\\android" ^
+  "-DCMAKE_SYSTEM_NAME=Android" ^
+  "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ^
+  "-DCMAKE_SYSTEM_VERSION=19" ^
+  "-DANDROID_PLATFORM=android-19" ^
+  "-DANDROID_ABI=x86_64" ^
+  "-DCMAKE_ANDROID_ARCH_ABI=x86_64" ^
+  "-DANDROID_NDK=C:\\Users\\MohamedAlaga\\AppData\\Local\\Android\\sdk\\ndk\\25.1.8937393" ^
+  "-DCMAKE_ANDROID_NDK=C:\\Users\\MohamedAlaga\\AppData\\Local\\Android\\sdk\\ndk\\25.1.8937393" ^
+  "-DCMAKE_TOOLCHAIN_FILE=C:\\Users\\MohamedAlaga\\AppData\\Local\\Android\\sdk\\ndk\\25.1.8937393\\build\\cmake\\android.toolchain.cmake" ^
+  "-DCMAKE_MAKE_PROGRAM=C:\\Users\\MohamedAlaga\\AppData\\Local\\Android\\sdk\\cmake\\3.18.1\\bin\\ninja.exe" ^
+  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=D:\\graduation project\\mobile_app\\aabkr\\build\\rive_common\\intermediates\\cxx\\Debug\\186u5d11\\obj\\x86_64" ^
+  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=D:\\graduation project\\mobile_app\\aabkr\\build\\rive_common\\intermediates\\cxx\\Debug\\186u5d11\\obj\\x86_64" ^
+  "-DCMAKE_BUILD_TYPE=Debug" ^
+  "-BC:\\Users\\MohamedAlaga\\AppData\\Local\\Pub\\Cache\\hosted\\pub.dev\\rive_common-0.2.8\\android\\.cxx\\Debug\\186u5d11\\x86_64" ^
+  -GNinja
